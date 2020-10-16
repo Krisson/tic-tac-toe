ce60461ba9c3c793db1c43efef96f2f910fb236d
Kristijan <kristijan.petkovski@gmail.com>
2020-10-16 20:45:02 +0200

    Updates

---
+++ precache-manifest.a319b6bb3ca8dadc13245a9d2066a8e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "612a160998512ad2e3e3fa4259e75fc7",
+    "url": "/ReactJS_test/index.html"
+  },
+  {
+    "revision": "470a316fc792e250d81b",
+    "url": "/ReactJS_test/static/css/main.3fa7586b.chunk.css"
+  },
+  {
+    "revision": "7b31bc9d3cf4cf1878d7",
+    "url": "/ReactJS_test/static/js/2.107327d8.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/ReactJS_test/static/js/2.107327d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "470a316fc792e250d81b",
+    "url": "/ReactJS_test/static/js/main.8d70cf51.chunk.js"
+  },
+  {
+    "revision": "fc5d883dfae36c9b42af",
+    "url": "/ReactJS_test/static/js/runtime-main.1fc0507c.js"
+  }
+]);
